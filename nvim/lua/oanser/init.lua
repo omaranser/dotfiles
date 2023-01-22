1d49bdb57e73f298f4a5229acb1a109ee0ba2809
oanser <omar.anser@inria.fr>
2023-01-22 21:10:06 +0100

    omar

--- nvim/lua/oanser/init.lua
+++ nvim/lua/oanser/init.lua
@@ -1,6 +1,7 @@
 require("oanser.set")
 require("oanser.plugins")
 require("oanser.autocommands")
+require("oanser.remap")
 
 --  Save file as sudo on files that require root permission
 vim.cmd("cabbrev w!! execute 'silent! write !sudo tee % >/dev/null' <bar> edit!")
---
+++ nvim/lua/oanser/remap.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = ","
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>o", ":setlocal spell! spelllang=en_us<CR>")
+vim.keymap.set("n", "c", '"_c')
+-- vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("v", "Q", "gq")
+vim.keymap.set("v", ".", ":normal .<CR>")
+
+vim.keymap.set("n", "<leader>s", ":!clear && shellcheck -x %<CR>")
+vim.keymap.set("n", "<leader>b", ":vsp<space>$BIB<CR>")
+vim.keymap.set("n", "<leader>r", ":vsp<space>$REFER<CR>")
+vim.keymap.set("n", "S", ":%s//g<Left><Left>")
+vim.keymap.set("n", "<leader>c", ':w! | !compiler "%:p"<CR>')
+vim.keymap.set("n", "<leader>p", ':!opout "%:p"<CR>')
+
+vim.keymap.set("n", "<leader>v", ":VimwikiIndex<CR>")
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/oanser/plugins.lua<CR>")
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
